846764b0d740e3bd1a54f3a1d55e0d674063ecff
Steven Sanséau <steste.etsets@gmail.com>
2017-12-21 10:27:01 +0100

    fix typo Addresses => Address

--- src/bin/server.js
+++ src/bin/server.js
@@ -1,14 +1,14 @@
 import { createServer } from '../lib/server'
 import { logger } from '../lib/logger'
 import { env } from '../lib/env'
-
+const port = env.PORT || process.env.PORT
 createServer().then(
   app => {
-    app.listen(process.env.PORT, () => {
-      const mode = process.env.NODE_ENV
+    app.listen(env.PORT, () => {
+      const mode = env.NODE_ENV
       logger.debug(
-        `Server listening on ${process.env.PORT} http://localhost:${
-          process.env.PORT
+        `Server listening on ${env.PORT} http://localhost:${
+          env.PORT
         }/ in ${mode} mode`
       )
     })
