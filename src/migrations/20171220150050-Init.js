37f468ddee10d11cb951bb6f907440e7f61c39d7
Steven Sanséau <steste.etsets@gmail.com>
2018-01-05 14:14:12 +0100

    update migration init file

--- src/migrations/20171220150050-Init.js
+++ src/migrations/20171220150050-Init.js
@@ -6,10 +6,13 @@ module.exports = {
       `CREATE TABLE IF NOT EXISTS "Client" ("client_id"  SERIAL , "aboweb_client_id" INTEGER, "email" VARCHAR(255) NOT NULL UNIQUE, "type_client" INTEGER, "first_name" VARCHAR(255), "last_name" VARCHAR(255), "login_code" INTEGER, "login_code_created_at" TIMESTAMP WITH TIME ZONE, "created_at" TIMESTAMP WITH TIME ZONE NOT NULL, "updated_at" TIMESTAMP WITH TIME ZONE NOT NULL, "deleted_at" TIMESTAMP WITH TIME ZONE, UNIQUE ("email"), PRIMARY KEY ("client_id"));`
     )
     queryInterface.sequelize.query(
-      `CREATE TABLE IF NOT EXISTS "Address" ("address_id"  SERIAL , "last_name" VARCHAR(255) NOT NULL, "first_name" VARCHAR(255) NOT NULL, "address" VARCHAR(255) NOT NULL, "address_post" VARCHAR(255), "address_pre" VARCHAR(255), "city" VARCHAR(255) NOT NULL, "phone" VARCHAR(255), "postal_code" VARCHAR(255) NOT NULL, "country" VARCHAR(255) NOT NULL, "company" VARCHAR(255), "type_address" VARCHAR(255), "created_at" TIMESTAMP WITH TIME ZONE NOT NULL, "updated_at" TIMESTAMP WITH TIME ZONE NOT NULL, "deleted_at" TIMESTAMP WITH TIME ZONE, "client_id" INTEGER REFERENCES "Client" ("client_id") ON DELETE SET NULL ON UPDATE CASCADE, PRIMARY KEY ("address_id"));`
+      `SELECT i.relname AS name, ix.indisprimary AS primary, ix.indisunique AS unique, ix.indkey AS indkey, array_agg(a.attnum) as column_indexes, array_agg(a.attname) AS column_names, pg_get_indexdef(ix.indexrelid) AS definition FROM pg_class t, pg_class i, pg_index ix, pg_attribute a WHERE t.oid = ix.indrelid AND i.oid = ix.indexrelid AND a.attrelid = t.oid AND t.relkind = 'r' and t.relname = 'Client' GROUP BY i.relname, ix.indexrelid, ix.indisprimary, ix.indisunique, ix.indkey ORDER BY i.relname;`
     )
     queryInterface.sequelize.query(
-      `SELECT i.relname AS name, ix.indisprimary AS primary, ix.indisunique AS unique, ix.indkey AS indkey, array_agg(a.attnum) as column_indexes, array_agg(a.attname) AS column_names, pg_get_indexdef(ix.indexrelid) AS definition FROM pg_class t, pg_class i, pg_index ix, pg_attribute a WHERE t.oid = ix.indrelid AND i.oid = ix.indexrelid AND a.attrelid = t.oid AND t.relkind = 'r' and t.relname = 'Client' GROUP BY i.relname, ix.indexrelid, ix.indisprimary, ix.indisunique, ix.indkey ORDER BY i.relname;`
+      `CREATE TABLE IF NOT EXISTS "Address" ("address_id"  SERIAL , "last_name" VARCHAR(255), "first_name" VARCHAR(255), "address" VARCHAR(255) NOT NULL, "address_post" VARCHAR(255), "address_pre" VARCHAR(255), "city" VARCHAR(255) NOT NULL, "phone" VARCHAR(255), "postal_code" VARCHAR(255) NOT NULL, "country" VARCHAR(255) NOT NULL, "company" VARCHAR(255), "type_address" VARCHAR(255), "address_equal" BOOLEAN, "aboweb_address_id" VARCHAR(255), "created_at" TIMESTAMP WITH TIME ZONE NOT NULL, "updated_at" TIMESTAMP WITH TIME ZONE NOT NULL, "deleted_at" TIMESTAMP WITH TIME ZONE, "client_id" INTEGER NOT NULL REFERENCES "Client" ("client_id") ON DELETE NO ACTION ON UPDATE CASCADE, PRIMARY KEY ("address_id"));`
+    )
+    queryInterface.sequelize.query(
+      `SELECT i.relname AS name, ix.indisprimary AS primary, ix.indisunique AS unique, ix.indkey AS indkey, array_agg(a.attnum) as column_indexes, array_agg(a.attname) AS column_names, pg_get_indexdef(ix.indexrelid) AS definition FROM pg_class t, pg_class i, pg_index ix, pg_attribute a WHERE t.oid = ix.indrelid AND i.oid = ix.indexrelid AND a.attrelid = t.oid AND t.relkind = 'r' and t.relname = 'Address' GROUP BY i.relname, ix.indexrelid, ix.indisprimary, ix.indisunique, ix.indkey ORDER BY i.relname;`
     )
     queryInterface.sequelize.query(
       `CREATE TABLE IF NOT EXISTS "Token" ("token_id"  SERIAL , "token_type" VARCHAR(255), "aboweb_id" VARCHAR(255), "stripe_token_id" VARCHAR(255), "stripe_customer_id" VARCHAR(255), "stripe_card_id" VARCHAR(255), "stripe_card_country" VARCHAR(255), "stripe_card_brand" VARCHAR(255), "stripe_card_cvc_check" VARCHAR(255), "stripe_card_exp_month" INTEGER, "stripe_card_exp_year" INTEGER, "stripe_card_last4" VARCHAR(255), "slimpay_rum_id" VARCHAR(255), "slimpay_token_id" VARCHAR(255), "slimpay_bic" VARCHAR(255), "slimpay_iban" VARCHAR(255), "slimpay_rum_code" VARCHAR(255), "created_at" TIMESTAMP WITH TIME ZONE NOT NULL, "updated_at" TIMESTAMP WITH TIME ZONE NOT NULL, "deleted_at" TIMESTAMP WITH TIME ZONE, "client_id" INTEGER REFERENCES "Client" ("client_id") ON DELETE SET NULL ON UPDATE CASCADE, PRIMARY KEY ("token_id"));`
@@ -18,35 +21,44 @@ module.exports = {
       `SELECT i.relname AS name, ix.indisprimary AS primary, ix.indisunique AS unique, ix.indkey AS indkey, array_agg(a.attnum) as column_indexes, array_agg(a.attname) AS column_names, pg_get_indexdef(ix.indexrelid) AS definition FROM pg_class t, pg_class i, pg_index ix, pg_attribute a WHERE t.oid = ix.indrelid AND i.oid = ix.indexrelid AND a.attrelid = t.oid AND t.relkind = 'r' and t.relname = 'Token' GROUP BY i.relname, ix.indexrelid, ix.indisprimary, ix.indisunique, ix.indkey ORDER BY i.relname;`
     )
     queryInterface.sequelize.query(
-      `CREATE TABLE IF NOT EXISTS "Offer" ("offer_id"  SERIAL , "name" VARCHAR(255), "aboweb_id" VARCHAR(255), "price_ttc" FLOAT, "monthly_price_ttc" FLOAT, "description" TEXT, "time_limited" BOOLEAN, "duration" INTEGER, "country_shipping" VARCHAR(255), "shipping_cost" FLOAT, "is_gift" BOOLEAN, "payment_method" INTEGER, "created_at" TIMESTAMP WITH TIME ZONE NOT NULL, "updated_at" TIMESTAMP WITH TIME ZONE NOT NULL, "deleted_at" TIMESTAMP WITH TIME ZONE, PRIMARY KEY ("offer_id"));`
+      `CREATE TABLE IF NOT EXISTS "Offer" ("offer_id"  SERIAL , "name" VARCHAR(255), "aboweb_id" VARCHAR(255), "price_ttc" FLOAT, "monthly_price_ttc" FLOAT, "description" TEXT, "time_limited" BOOLEAN, "duration" INTEGER, "country_shipping" VARCHAR(255), "shipping_cost" FLOAT, "is_gift" BOOLEAN, "is_free" BOOLEAN, "is_free_gift" BOOLEAN, "payment_method" INTEGER, "created_at" TIMESTAMP WITH TIME ZONE NOT NULL, "updated_at" TIMESTAMP WITH TIME ZONE NOT NULL, "deleted_at" TIMESTAMP WITH TIME ZONE, PRIMARY KEY ("offer_id"));`
+    )
+    queryInterface.sequelize.query(
+      `SELECT i.relname AS name, ix.indisprimary AS primary, ix.indisunique AS unique, ix.indkey AS indkey, array_agg(a.attnum) as column_indexes, array_agg(a.attname) AS column_names, pg_get_indexdef(ix.indexrelid) AS definition FROM pg_class t, pg_class i, pg_index ix, pg_attribute a WHERE t.oid = ix.indrelid AND i.oid = ix.indexrelid AND a.attrelid = t.oid AND t.relkind = 'r' and t.relname = 'Offer' GROUP BY i.relname, ix.indexrelid, ix.indisprimary, ix.indisunique, ix.indkey ORDER BY i.relname;`
+    )
+    queryInterface.sequelize.query(
+      `CREATE TABLE IF NOT EXISTS "Checkout" ("checkout_id"  SERIAL , "checkout_step" INTEGER, "aboweb_client_id" INTEGER, "aboweb_subscribe_id" INTEGER, "payment_method" INTEGER, "is_gift" BOOLEAN, "status" VARCHAR(255), "cgv_accepted" BOOLEAN, "source" VARCHAR(255), "created_at" TIMESTAMP WITH TIME ZONE NOT NULL, "updated_at" TIMESTAMP WITH TIME ZONE NOT NULL, "deleted_at" TIMESTAMP WITH TIME ZONE, "address_id" INTEGER REFERENCES "Address" ("address_id") ON DELETE SET NULL ON UPDATE CASCADE, "client_id" INTEGER REFERENCES "Client" ("client_id") ON DELETE SET NULL ON UPDATE CASCADE, "token_id" INTEGER REFERENCES "Token" ("token_id") ON DELETE SET NULL ON UPDATE CASCADE, "invoice_address_id" INTEGER REFERENCES "Address" ("address_id") ON DELETE SET NULL ON UPDATE CASCADE, "delivery_address_id" INTEGER REFERENCES "Address" ("address_id") ON DELETE SET NULL ON UPDATE CASCADE, "offer_id" INTEGER REFERENCES "Offer" ("offer_id") ON DELETE SET NULL ON UPDATE CASCADE, PRIMARY KEY ("checkout_id"));`
+    )
+    queryInterface.sequelize.query(
+      `SELECT i.relname AS name, ix.indisprimary AS primary, ix.indisunique AS unique, ix.indkey AS indkey, array_agg(a.attnum) as column_indexes, array_agg(a.attname) AS column_names, pg_get_indexdef(ix.indexrelid) AS definition FROM pg_class t, pg_class i, pg_index ix, pg_attribute a WHERE t.oid = ix.indrelid AND i.oid = ix.indexrelid AND a.attrelid = t.oid AND t.relkind = 'r' and t.relname = 'Checkout' GROUP BY i.relname, ix.indexrelid, ix.indisprimary, ix.indisunique, ix.indkey ORDER BY i.relname;`
+    )
+
+    queryInterface.sequelize.query(
+      `CREATE TABLE IF NOT EXISTS "Cron" ("cron_id"  SERIAL , "type" VARCHAR(255), "data" JSON, "created_at" TIMESTAMP WITH TIME ZONE NOT NULL, "updated_at" TIMESTAMP WITH TIME ZONE NOT NULL, "deleted_at" TIMESTAMP WITH TIME ZONE, PRIMARY KEY ("cron_id"));`
+    )
+    queryInterface.sequelize.query(
+      `SELECT i.relname AS name, ix.indisprimary AS primary, ix.indisunique AS unique, ix.indkey AS indkey, array_agg(a.attnum) as column_indexes, array_agg(a.attname) AS column_names, pg_get_indexdef(ix.indexrelid) AS definition FROM pg_class t, pg_class i, pg_index ix, pg_attribute a WHERE t.oid = ix.indrelid AND i.oid = ix.indexrelid AND a.attrelid = t.oid AND t.relkind = 'r' and t.relname = 'Cron' GROUP BY i.relname, ix.indexrelid, ix.indisprimary, ix.indisunique, ix.indkey ORDER BY i.relname;`
     )
     queryInterface.sequelize.query(
       `CREATE TABLE IF NOT EXISTS "Newsletter" ("email" VARCHAR(255) NOT NULL , "name" VARCHAR(255), "created_at" TIMESTAMP WITH TIME ZONE NOT NULL, "updated_at" TIMESTAMP WITH TIME ZONE NOT NULL, "deleted_at" TIMESTAMP WITH TIME ZONE, PRIMARY KEY ("email"));`
     )
     queryInterface.sequelize.query(
-      `CREATE TABLE IF NOT EXISTS "Sponsor" ("sponsor_id"  SERIAL , "client_sponsor_id" INTEGER, "code" VARCHAR(255), "checkout_id" INTEGER, "created_at" TIMESTAMP WITH TIME ZONE NOT NULL, "updated_at" TIMESTAMP WITH TIME ZONE NOT NULL, "deleted_at" TIMESTAMP WITH TIME ZONE, PRIMARY KEY ("sponsor_id"));`
-    )
-    queryInterface.sequelize.query(
-      `CREATE TABLE IF NOT EXISTS "Subscription" ("subscription_id"  SERIAL , "client_id" INTEGER, "aboweb_id" INTEGER, "checkout_id" INTEGER, "offer_id" INTEGER, "token_id" INTEGER, "address_delivery_id" INTEGER, "address_invoice_id" INTEGER, "voucher_id" INTEGER, "duration" INTEGER, "created_at" TIMESTAMP WITH TIME ZONE NOT NULL, "updated_at" TIMESTAMP WITH TIME ZONE NOT NULL, "deleted_at" TIMESTAMP WITH TIME ZONE, PRIMARY KEY ("subscription_id"));`
-    )
-    queryInterface.sequelize.query(
-      `CREATE TABLE IF NOT EXISTS "Voucher" ("voucher_id"  SERIAL , "abboweb_id" INTEGER, "voucher" VARCHAR(255) NOT NULL, "start_date" TIMESTAMP WITH TIME ZONE, "end_date" TIMESTAMP WITH TIME ZONE, "created_at" TIMESTAMP WITH TIME ZONE NOT NULL, "updated_at" TIMESTAMP WITH TIME ZONE NOT NULL, "deleted_at" TIMESTAMP WITH TIME ZONE, PRIMARY KEY ("voucher_id"));`
-    )
-    queryInterface.sequelize.query(
-      `SELECT i.relname AS name, ix.indisprimary AS primary, ix.indisunique AS unique, ix.indkey AS indkey, array_agg(a.attnum) as column_indexes, array_agg(a.attname) AS column_names, pg_get_indexdef(ix.indexrelid) AS definition FROM pg_class t, pg_class i, pg_index ix, pg_attribute a WHERE t.oid = ix.indrelid AND i.oid = ix.indexrelid AND a.attrelid = t.oid AND t.relkind = 'r' and t.relname = 'Address' GROUP BY i.relname, ix.indexrelid, ix.indisprimary, ix.indisunique, ix.indkey ORDER BY i.relname;`
-    )
-    queryInterface.sequelize.query(
-      `SELECT i.relname AS name, ix.indisprimary AS primary, ix.indisunique AS unique, ix.indkey AS indkey, array_agg(a.attnum) as column_indexes, array_agg(a.attname) AS column_names, pg_get_indexdef(ix.indexrelid) AS definition FROM pg_class t, pg_class i, pg_index ix, pg_attribute a WHERE t.oid = ix.indrelid AND i.oid = ix.indexrelid AND a.attrelid = t.oid AND t.relkind = 'r' and t.relname = 'Offer' GROUP BY i.relname, ix.indexrelid, ix.indisprimary, ix.indisunique, ix.indkey ORDER BY i.relname;`
-    )
-
-    queryInterface.sequelize.query(
       `SELECT i.relname AS name, ix.indisprimary AS primary, ix.indisunique AS unique, ix.indkey AS indkey, array_agg(a.attnum) as column_indexes, array_agg(a.attname) AS column_names, pg_get_indexdef(ix.indexrelid) AS definition FROM pg_class t, pg_class i, pg_index ix, pg_attribute a WHERE t.oid = ix.indrelid AND i.oid = ix.indexrelid AND a.attrelid = t.oid AND t.relkind = 'r' and t.relname = 'Newsletter' GROUP BY i.relname, ix.indexrelid, ix.indisprimary, ix.indisunique, ix.indkey ORDER BY i.relname;`
+    )
+    queryInterface.sequelize.query(
+      `CREATE TABLE IF NOT EXISTS "Sponsor" ("sponsor_id"  SERIAL , "client_sponsor_id" INTEGER, "code" VARCHAR(255), "checkout_id" INTEGER, "created_at" TIMESTAMP WITH TIME ZONE NOT NULL, "updated_at" TIMESTAMP WITH TIME ZONE NOT NULL, "deleted_at" TIMESTAMP WITH TIME ZONE, PRIMARY KEY ("sponsor_id"));`
     )
     queryInterface.sequelize.query(
       `SELECT i.relname AS name, ix.indisprimary AS primary, ix.indisunique AS unique, ix.indkey AS indkey, array_agg(a.attnum) as column_indexes, array_agg(a.attname) AS column_names, pg_get_indexdef(ix.indexrelid) AS definition FROM pg_class t, pg_class i, pg_index ix, pg_attribute a WHERE t.oid = ix.indrelid AND i.oid = ix.indexrelid AND a.attrelid = t.oid AND t.relkind = 'r' and t.relname = 'Sponsor' GROUP BY i.relname, ix.indexrelid, ix.indisprimary, ix.indisunique, ix.indkey ORDER BY i.relname;`
     )
     queryInterface.sequelize.query(
+      `CREATE TABLE IF NOT EXISTS "Subscription" ("subscription_id"  SERIAL , "aboweb_subscription_id" INTEGER, "aboweb_client_id" INTEGER, "first_number_delivered" INTEGER, "last_number_delivered" INTEGER, "is_invoiced" BOOLEAN, "is_suspended" BOOLEAN, "is_resubscription" BOOLEAN, "free_subscription" BOOLEAN, "number_of_copy" INTEGER, "order_number" VARCHAR(255), "created_at" TIMESTAMP WITH TIME ZONE NOT NULL, "updated_at" TIMESTAMP WITH TIME ZONE NOT NULL, "deleted_at" TIMESTAMP WITH TIME ZONE, PRIMARY KEY ("subscription_id"));`
+    )
+    queryInterface.sequelize.query(
       `SELECT i.relname AS name, ix.indisprimary AS primary, ix.indisunique AS unique, ix.indkey AS indkey, array_agg(a.attnum) as column_indexes, array_agg(a.attname) AS column_names, pg_get_indexdef(ix.indexrelid) AS definition FROM pg_class t, pg_class i, pg_index ix, pg_attribute a WHERE t.oid = ix.indrelid AND i.oid = ix.indexrelid AND a.attrelid = t.oid AND t.relkind = 'r' and t.relname = 'Subscription' GROUP BY i.relname, ix.indexrelid, ix.indisprimary, ix.indisunique, ix.indkey ORDER BY i.relname;`
+    )
+    queryInterface.sequelize.query(
+      `CREATE TABLE IF NOT EXISTS "Voucher" ("voucher_id"  SERIAL , "abboweb_id" INTEGER, "voucher" VARCHAR(255) NOT NULL, "start_date" TIMESTAMP WITH TIME ZONE, "end_date" TIMESTAMP WITH TIME ZONE, "created_at" TIMESTAMP WITH TIME ZONE NOT NULL, "updated_at" TIMESTAMP WITH TIME ZONE NOT NULL, "deleted_at" TIMESTAMP WITH TIME ZONE, PRIMARY KEY ("voucher_id"));`
     )
     queryInterface.sequelize.query(
       `SELECT i.relname AS name, ix.indisprimary AS primary, ix.indisunique AS unique, ix.indkey AS indkey, array_agg(a.attnum) as column_indexes, array_agg(a.attname) AS column_names, pg_get_indexdef(ix.indexrelid) AS definition FROM pg_class t, pg_class i, pg_index ix, pg_attribute a WHERE t.oid = ix.indrelid AND i.oid = ix.indexrelid AND a.attrelid = t.oid AND t.relkind = 'r' and t.relname = 'Voucher' GROUP BY i.relname, ix.indexrelid, ix.indisprimary, ix.indisunique, ix.indkey ORDER BY i.relname;`
--- src/migrations/20171220150090-create-charges-table.js
+++ src/migrations/20171220150090-create-charges-table.js
@@ -8,7 +8,6 @@ module.exports = {
     queryInterface.sequelize.query(
       `SELECT i.relname AS name, ix.indisprimary AS primary, ix.indisunique AS unique, ix.indkey AS indkey, array_agg(a.attnum) as column_indexes, array_agg(a.attname) AS column_names, pg_get_indexdef(ix.indexrelid) AS definition FROM pg_class t, pg_class i, pg_index ix, pg_attribute a WHERE t.oid = ix.indrelid AND i.oid = ix.indexrelid AND a.attrelid = t.oid AND t.relkind = 'r' and t.relname = 'Charge' GROUP BY i.relname, ix.indexrelid, ix.indisprimary, ix.indisunique, ix.indkey ORDER BY i.relname;`
     )
-
     return queryInterface
   },
 
