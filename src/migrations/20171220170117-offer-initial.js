2933d34cac6553012be648099f6bc5ff069e63e2
Steven Sanséau <steste.etsets@gmail.com>
2018-01-06 15:02:59 +0100

    change migrate file

--- src/migrations/20171220170117-offer-initial.js
+++ src/migrations/20171220170117-offer-initial.js
@@ -62,7 +62,7 @@ module.exports = {
       {
         name: 'abo-duree-libre-5-euro-belgique-sepa',
         aboweb_id: 'F-EB-ETR1-900-4-DL-P',
-        price_ttc: 500,
+        price_ttc: 900,
         monthly_price_ttc: 5,
         description:
           'Abonnement personnel à durée libre, 4 numéro par mois pour 5€ par mois paiement par SEPA, livraison en belgique',
