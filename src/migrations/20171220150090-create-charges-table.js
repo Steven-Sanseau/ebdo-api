7d0613626125aef48a7f7da7ecd40b19afd99803
Thibault Lenclos <thibzy@gmail.com>
2018-01-10 10:34:17 +0100

    Fix migrations

--- src/migrations/20171220150090-create-charges-table.js
+++ src/migrations/20171220150090-create-charges-table.js
@@ -5,9 +5,6 @@ module.exports = {
     queryInterface.sequelize.query(
       `CREATE TABLE IF NOT EXISTS "Charge" ("charge_id"  SERIAL , "stripe_charge_return" JSON, "created_at" TIMESTAMP WITH TIME ZONE NOT NULL, "updated_at" TIMESTAMP WITH TIME ZONE NOT NULL, "deleted_at" TIMESTAMP WITH TIME ZONE, "checkout_id" INTEGER REFERENCES "Checkout" ("checkout_id") ON DELETE SET NULL ON UPDATE CASCADE, "token_id" INTEGER REFERENCES "Token" ("token_id") ON DELETE SET NULL ON UPDATE CASCADE, "client_id" INTEGER REFERENCES "Client" ("client_id") ON DELETE SET NULL ON UPDATE CASCADE, PRIMARY KEY ("charge_id"));`
     )
-    queryInterface.sequelize.query(
-      `SELECT i.relname AS name, ix.indisprimary AS primary, ix.indisunique AS unique, ix.indkey AS indkey, array_agg(a.attnum) as column_indexes, array_agg(a.attname) AS column_names, pg_get_indexdef(ix.indexrelid) AS definition FROM pg_class t, pg_class i, pg_index ix, pg_attribute a WHERE t.oid = ix.indrelid AND i.oid = ix.indexrelid AND a.attrelid = t.oid AND t.relkind = 'r' and t.relname = 'Charge' GROUP BY i.relname, ix.indexrelid, ix.indisprimary, ix.indisunique, ix.indkey ORDER BY i.relname;`
-    )
     return queryInterface
   },
 
