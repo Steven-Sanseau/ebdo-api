e0f650beb72873b3f0b3053be18640d64f94dea9
Steven Sanséau <steste.etsets@gmail.com>
2018-01-12 02:40:49 +0100

    MEP

--- src/middleware/notFound.js
+++ src/middleware/notFound.js
@@ -1,7 +1,6 @@
 export async function notFoundHandler(ctx) {
   const msg = `${ctx.request.method} ${ctx.request.path}`
   ctx.notFound({
-    message: `No endpoint matched your request: ${msg}`
+    message: `Hello :)`
   })
 }
-
